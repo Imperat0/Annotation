493e28119c2b07d8fc45ed795603b960bbbff6f8
Daniel Imperato <80261360+Imperat0@users.noreply.github.com>
2023-10-14 16:44:45 -0300

    Atividade Ebac

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    @Tabela
+    public void anotacaoTabela() {
+        System.out.println("Minha primeira tabela");
+    }
+}
